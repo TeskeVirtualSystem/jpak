abda49fff1e494998d09df337e9f754914366d39
Lucas Teske <lucas@teske.net.br>
2013-10-29 21:45:25 -0200

    Update zip.js

--- js/zip.js
+++ js/zip.js
@@ -1,8 +1,26 @@
 /* This is a modified zip.js file from JSXGraph 
     The original code can be found at: https://github.com/jsxgraph/jsxgraph
-    And the license is: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
-
-    Modifications to adapt to JPAK Project
+    The JSXGraph license:
+    
+    #################################################################################
+    
+    JSXGraph is free software dual licensed under the GNU LGPL or MIT License.
+    
+    You can redistribute it and/or modify it under the terms of the
+    
+    GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version OR
+    MIT License: https://github.com/jsxgraph/jsxgraph/blob/master/LICENSE.MIT
+    JSXGraph is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License and the MIT License along with JSXGraph. If not, see http://www.gnu.org/licenses/ and http://opensource.org/licenses/MIT/.
+    #################################################################################
+    
+    Also,  JSX can be licensed as Apache 2.0 as I was spoken here:
+    https://groups.google.com/forum/#!topic/jsxgraph/pryVzPsgmdE
+    
+    Look the JSXGraph Github for more infos about this code license.
+    
+    Modifications made to adapt to JPAK Project
 */
 
 
