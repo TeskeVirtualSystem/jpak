46ea28216ce826dcf4d829bf2f47add1682bc69d
Lucas Teske <lucas@teske.net.br>
2015-10-31 22:43:34 -0200

    Fix

--- js/jpak.js
+++ js/jpak.js
@@ -1,4 +1,3 @@
-
 /*
      _ ____   _    _  __        _   ___  
     | |  _ \ / \  | |/ / __   _/ | / _ \ 
@@ -10,6 +9,13 @@ Multiuse Javascript Package
 By: Lucas Teske
 https://github.com/racerxdl/jpak
 */
+
+
+/**
+ * JPAK Base Class
+ * @expose
+ */
+var JPAK = function()   {};
 
 /** 
  * Base64 Enconding Base
@@ -39,7 +45,7 @@ if(!ArrayBuffer.prototype.slice)    {
         }
         return arr;
     };
-};
+}
 
 /**
  * Clean all deletedValue from array
@@ -47,21 +53,13 @@ if(!ArrayBuffer.prototype.slice)    {
  */
 Array.prototype.clean = function(deleteValue) {
   for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
+    if (this[i] === deleteValue) {         
       this.splice(i, 1);
       i--;
     }
   }
   return this;
 };
-
-/**
- * JPAK Base Class
- * @expose
- */
-var JPAK = function()   {};
-
-window["JPAK"] = JPAK;
 
 /**
  * Convert Unsigned Int8 ArrayBuffer to String
@@ -73,7 +71,7 @@ JPAK.Uint8ArrayToString = function(uintArray) {
     for(var i=0;i<uintArray.byteLength;i++)  
         o += String.fromCharCode(uintArray[i]);
     return o;
-}
+};
 
 /** 
  * Provided for retro-compatibility.
@@ -103,54 +101,54 @@ JPAK.String2ArrayBuffer = function(str)   {
  * @return {string} base64
  */
 JPAK.ArrayBufferToBase64 = function(arrayBuffer)  {
-  var base64    = ''
+  var base64    = '';
 
-  var bytes         = new Uint8Array(arrayBuffer)
-  var byteLength    = bytes.byteLength
-  var byteRemainder = byteLength % 3
-  var mainLength    = byteLength - byteRemainder
+  var bytes         = new Uint8Array(arrayBuffer);
+  var byteLength    = bytes.byteLength;
+  var byteRemainder = byteLength % 3;
+  var mainLength    = byteLength - byteRemainder;
 
-  var a, b, c, d
-  var chunk
+  var a, b, c, d;
+  var chunk;
 
   // Main loop deals with bytes in chunks of 3
   for (var i = 0; i < mainLength; i = i + 3) {
     // Combine the three bytes into a single integer
-    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
 
     // Use bitmasks to extract 6-bit segments from the triplet
-    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-    b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
-    c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
-    d = chunk & 63               // 63       = 2^6 - 1
+    a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048)   >> 12; // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032)     >>  6; // 4032     = (2^6 - 1) << 6
+    d = chunk & 63;               // 63       = 2^6 - 1
 
     // Convert the raw binary segments to the appropriate ASCII encoding
-    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + JPAK.Base64_Encoding[c] + JPAK.Base64_Encoding[d]
+    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + JPAK.Base64_Encoding[c] + JPAK.Base64_Encoding[d];
   }
 
   // Deal with the remaining bytes and padding
-  if (byteRemainder == 1) {
-    chunk = bytes[mainLength]
+  if (byteRemainder === 1) {
+    chunk = bytes[mainLength];
 
-    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+    a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
 
     // Set the 4 least significant bits to zero
-    b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+    b = (chunk & 3)   << 4; // 3   = 2^2 - 1
 
-    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + '=='
-  } else if (byteRemainder == 2) {
-    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + '==';
+  } else if (byteRemainder === 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
 
-    a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-    b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+    a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008)  >>  4; // 1008  = (2^6 - 1) << 4
 
     // Set the 2 least significant bits to zero
-    c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+    c = (chunk & 15)    <<  2; // 15    = 2^4 - 1
 
-    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + JPAK.Base64_Encoding[c] + '='
+    base64 += JPAK.Base64_Encoding[a] + JPAK.Base64_Encoding[b] + JPAK.Base64_Encoding[c] + '=';
   }
 
-  return base64
+  return base64;
 };
 /**
  * Logs a message, if enabled
@@ -160,7 +158,7 @@ JPAK.ArrayBufferToBase64 = function(arrayBuffer)  {
 JPAK.log = function(msg)    {
     if(JPAK.ShowMessages)
         console.log(msg);
-}
+};
 
 /**
  * Constructor of JPAKLoader
@@ -185,7 +183,7 @@ JPAK.jpakloader = function(parameters)  {
  */
 JPAK.jpakloader.prototype.CacheLoad     =   function(path)  {
     for(var i=0;i<this.filecache.length;i++)    {
-        if(this.filecache[i].path == path)
+        if(this.filecache[i].path === path)
             return this.filecache[i];
     }
     return undefined;
@@ -208,18 +206,18 @@ JPAK.jpakloader.prototype.Load = function() {
         
         //  The On Progress request. For now it only does stuff if there is a hooked onprogress jpakloader event.
         xhr.onprogress = function(e)    {
-            if (e.lengthComputable && _this.onprogress != undefined)     {  
+            if (e.lengthComputable && _this.onprogress !== undefined)     {  
                 var percentComplete = (( (e.loaded / e.total)*10000 ) >> 0)/100;  // Rounded percent to two decimal
                     _this.onprogress({"loaded":e.loaded,"total":e.total,"percent": percentComplete});                
             }   
-        }
+        };
         
         //  The onload function. This parses the JPAK and after loading the filetable it calls the onload event of jpakloader
         xhr.onload = function(e) {
-            if (this.status == 200) {
+            if (this.status === 200) {
                 var data = this.response;
                 var MagicNumber = u8as(new Uint8Array(data.slice(0,5)));
-                if(MagicNumber == "JPAK1")  {
+                if(MagicNumber === "JPAK1")  {
                     JPAK.log("JPAK::jpakloader - Loaded file "+_this.jpakfile+" successfully. JPAK1 Format");
                     var filetableoffset = new DataView(data.slice(data.byteLength-4,data.byteLength)).getUint32(0, true);
                     var filetable = new Uint8Array(data.slice(filetableoffset,data.byteLength-4));
@@ -227,20 +225,20 @@ JPAK.jpakloader.prototype.Load = function() {
                     _this.filetable = filetable;
                     _this.jpakdata = data;
                     _this.dataloaded = true;
-                    if(_this.onload != undefined)   
+                    if(_this.onload !== undefined)   
                         _this.onload();
                 }else{
                     JPAK.log("JPAK::jpakloader - Error loading file "+_this.jpakfile+" (8000): Wrong File Magic. Expected JPAK1 got "+MagicNumber);
-                    if(_this.onerror != undefined)
+                    if(_this.onerror !== undefined)
                         _this.onerror({"text": "Wrong File Magic. Expected JPAK1 got "+MagicNumber, "errorcode" : 8000}); 
                 }
             }
         };
         xhr.onreadystatechange = function (aEvt) {
-            if (this.readyState == 4) {
-                if(this.status != 200)   {
+            if (this.readyState === 4) {
+                if(this.status !== 200)   {
                     JPAK.log("JPAK::jpakloader - Error loading file "+_this.jpakfile+" ("+this.status+"): "+this.statusText);
-                    if(_this.onerror != undefined)
+                    if(_this.onerror !== undefined)
                         _this.onerror({"text": this.statusText, "errorcode": this.status});
                 }
           }
@@ -260,7 +258,7 @@ JPAK.jpakloader.prototype.Load = function() {
 JPAK.jpakloader.prototype.FindDirectoryEntry = function(path)   {
     var base = this.filetable;
     if(this.dataloaded) {
-        if(path != "/") {
+        if(path !== "/") {
             path = path.split("/").clean("");
             var dir = "", ok = true;
             for(var i=0;i<path.length;i++)    {
@@ -290,7 +288,7 @@ JPAK.jpakloader.prototype.FindFileEntry = function(path)    {
     var filename  = pathblock[pathblock.length-1];
     path = path.replace(filename,"");
     var base = this.FindDirectoryEntry(path);
-    if(base != undefined)   
+    if(base !== undefined)   
         if(filename in base.files)  
             return base.files[filename];
     return undefined;
@@ -306,10 +304,10 @@ JPAK.jpakloader.prototype.ls = function(path)   {
     var out = { "files" : [], "dirs" : [] };
     if(this.dataloaded) {
         var base = this.FindDirectoryEntry(path);
-        if(base != undefined)  {
+        if(base !== undefined)  {
             for(var i in base.files)
                 out.files.push(base.files[i]);
-            for(var i in base.directories)
+            for(i in base.directories)
                 out.dirs.push({"name" : base.directories[i].name, "numfiles": base.directories[i].numfiles});
         }else
             out.error = "Directory not found!";
@@ -331,7 +329,7 @@ JPAK.jpakloader.prototype.GetFile = function(path, type)  {
     type = type || 'application/octet-binary';
     var cache = this.CacheLoad(path);
     
-    if(file != undefined && cache == undefined)  { 
+    if(file !== undefined && cache === undefined)  { 
         //  Add it to file cache
         var dataslice = this.jpakdata.slice(file.offset,file.offset+file.size);
         if(file.compressed !== undefined && file.compressed)
@@ -339,7 +337,7 @@ JPAK.jpakloader.prototype.GetFile = function(path, type)  {
         var blob = new Blob([new Uint8Array(dataslice).buffer], { "type":type});
         this.filecache.push({"path":path,"type":type,"blob":blob,"url":URL.createObjectURL(blob), "arraybuffer" : dataslice} );
         return blob;
-    }else if(cache != undefined)
+    }else if(cache !== undefined)
         return cache.blob;
         
     return undefined;
@@ -354,9 +352,9 @@ JPAK.jpakloader.prototype.GetFile = function(path, type)  {
  */
 JPAK.jpakloader.prototype.GetFileURL = function(path, type) {
     var cache = this.CacheLoad(path);
-    if(cache == undefined)  {
+    if(cache === undefined)  {
         var blob = this.GetFile(path, type);
-        if(blob != undefined)   
+        if(blob !== undefined)   
             return URL.createObjectURL(blob);   
         else{
             JPAK.log("Error: Cannot find file \""+path+"\"");   
@@ -378,7 +376,7 @@ JPAK.jpakloader.prototype.GetFileArrayBuffer = function(path, type) {
     type = type || 'application/octet-binary';
     var cache = this.CacheLoad(path);
     
-    if(file != undefined && cache == undefined)  { 
+    if(file !== undefined && cache === undefined)  { 
         //  Add it to file cache
         var dataslice = this.jpakdata.slice(file.offset,file.offset+file.size);
         if(file.compressed !== undefined && file.compressed)
@@ -386,7 +384,7 @@ JPAK.jpakloader.prototype.GetFileArrayBuffer = function(path, type) {
         var blob = new Blob([new Uint8Array(dataslice).buffer], { "type":type});
         this.filecache.push({"path":path,"type":type,"blob":blob,"url":URL.createObjectURL(blob), "arraybuffer" : dataslice});
         return dataslice;
-    }else if(cache != undefined)
+    }else if(cache !== undefined)
         return cache.arraybuffer;
     
     JPAK.log("Error: Cannot find file \""+path+"\"");    
@@ -403,7 +401,7 @@ JPAK.jpakloader.prototype.GetFileArrayBuffer = function(path, type) {
  */
 JPAK.jpakloader.prototype.GetBase64File = function(path, type) {
     var filedata = this.GetFileArrayBuffer(path, type);
-    if(filedata == undefined)
+    if(filedata === undefined)
         return undefined;
     
     return JPAK.ArrayBufferToBase64(filedata);
@@ -422,10 +420,13 @@ JPAK.jpakloader.prototype.GetHTMLDataURIFile = function(path, type, encoding) {
     var b64 = this.GetBase64File(path, type);
     // HTML Data URI Format: data:[<MIME-type>][;charset=<encoding>][;base64],<data>
     if(b64 === undefined)
-        return undefined
+        return undefined;
         
     if(encoding !== undefined)
         return "data:"+type+";charset="+encoding+";base64,"+b64;
     else
         return "data:"+type+";base64,"+b64;
 };
+
+
+window.JPAK = JPAK;
